1e3ef5608e35b742704ebf9a7f60ba968d0a431d
Philip Dai <smartielion@gmail.com>
2015-10-18 17:55:16 -0500

    Set up chai and mocha testing suite

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,19 @@ gulp.task('serve', function() {
 	gulp.watch(['app/**/*.*'])
 		.on('change', browserSync.reload);
 });
+
+gulp.task('serve-test', function() {
+	browserSync.init({
+		notify: false,
+		port: 8081,
+		server: {
+			baseDir: ["test", "app"],
+			routes:{
+				'/bower_components': 'bower_components'
+			}
+		}
+	});
+	
+	gulp.watch(['app/**/*.*'])
+		.on('change', browserSync.reload);
+});
