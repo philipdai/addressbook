3e041607a97982e2273440b823dfe79b62c5f0b2
Philip Dai <smartielion@gmail.com>
2015-10-19 15:40:47 -0500

    Add Istanbul text coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,22 @@
 module.exports = function(config) {
 	config.set({
+		//plugins: ['karma-mocha', 'karma-phantomjs-launcher'],
 		browsers: ['PhantomJS'],
 		frameworks: ['mocha'],
+		preprocessors: {
+			'app/**/*.js': ['coverage']
+		},
+		coverageReporter: {
+			includeAllSources: true,
+			reporters: [{
+				type: 'text'
+			}]
+		},
 		files: [
 			"bower_components/angular/angular.js",
+			"bower_components/angular-mocks/angular-mocks.js",
 			"bower_components/chai/chai.js",
-//			"bower_components/mocha/mocha.js",
+
 			"app/**/*.js",		
 			"test/*.js"
 		]
