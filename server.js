ab1ab482f4fed1d92a015569d88113cf82533c9f
Philip Dai <smartielion@gmail.com>
2015-10-20 02:25:43 -0500

    Improve contact list page

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ var cors = require('cors');
 var app = express();
 app.use(cors());
 
-var contacts = [
-	{
-		name: "Eddard"
-	}, {
-		name: "Robert"
-	}
-];
+var contacts = [{
+    "name":"Shotaro Kaneda",
+    "age":16,
+    "occupation":"Futuristic Biker Gang Captain",
+    "email":"kaneda@capsules.co.jp"
+},{
+    "name":"Jon Snow",
+    "age":15,
+    "occupation":"Lord Commander of the Wall",
+    "email":"jon@nightswatch.wl"
+},{
+    "name":"Lara Croft",
+    "age":22,
+    "occupation":"Tomb Raider",
+    "email":"lara@croft.co.uk"
+}];
 
 app.get('/contacts', function(req, res) {
 	res.status(200).json(contacts);
